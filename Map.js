07e4723b89f9ff494f7914c08fe5932478f3f1d6
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-27 17:34:39 +0300

    Update Map.js map draw

--- Map.js
+++ Map.js
@@ -1,21 +1,47 @@
 export default class TileMap {
     constructor(tileSize){
         this.tileSize = tileSize;
+
+        this.yellowDot = new Image()
+        this.yellowDot.src = "../img/yellowDot.png";
+
+        this.wall = new Image()
+        this.wall.src = "../img/wall.png";
+
     }
 
-    Map = [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1],
+    map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     
     
     ];
-    draw(){
-//console.log("draw");
+
+    draw(ctx){
+    for(let row = 0; row < this.map.length; row++) {
+    for(let column = 0; column < this.map[row].length; column++ ) {
+    let tile = this.map[row] [column];
+    if(tile === 1 )  {
+        this.#drawWall(ctx, column, row, this.tileSize);
     }
+}}}
+#drawWall(ctx, column, row, size){
+    ctx.drawImage(
+        this.wall,
+        column * this.tileSize,
+        row * this.tileSize,
+        size,
+        size
+        );
 }
+    }
+    setCanvasSize(canvas);{
+        canvas.width = this.map[0].length * this.tileSize;
+        canvas.height = this.map.lenght * this.tileSize;
+    }
