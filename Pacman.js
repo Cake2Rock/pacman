99843a08bd858b181487ef8c48322aed3c3363f4
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-27 21:31:42 +0300

    Update Pacman.js
    Starting to make looping anim for pacman

--- Pacman.js
+++ Pacman.js
@@ -6,3 +6,25 @@ export default class Pacman {
       this.velocity = velocity;
       this.tileMap = tileMap;
     }
+    draw(ctx) {}
+
+    #loadPacmanImages() {
+        const pacmanImage1 = new Image();
+        pacmanImage1.src = "img/pac0.png";
+    
+        const pacmanImage2 = new Image();
+        pacmanImage2.src = "img/pac1.png";
+    
+        const pacmanImage3 = new Image();
+        pacmanImage3.src = "img/pac2.png";
+    
+        const pacmanImage4 = new Image();
+        pacmanImage4.src = "img/pac1.png";
+    
+        this.pacmanImages = [
+          pacmanImage1,
+          pacmanImage2,
+          pacmanImage3,
+          pacmanImage4,
+        ];
+    
