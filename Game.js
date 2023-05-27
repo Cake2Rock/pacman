a30413a318c37e2769bae8f2dd9267759a356036
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-27 17:34:06 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,11 +1,15 @@
 import TileMap from "./Map.js";
 
 const tileSize = 32;
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext ("2d");
-const tileMap = new TileMap (tileSize);
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const tileMap = new TileMap(tileSize);
 
 function gameloop ( ){ 
-    console.log ("gamer loop");
+    console.log("here");
+    tileMap.draw (ctx);
+
 }
-setInterval(gameloop, 1000/75);
+
+tileMap.setCanvasSize(canvas);
+setInterval(gameLoop, 1000 / 75);
