b7414726becb346c961d549c8a81295af6c903b9
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-27 22:21:57 +0300

    req and cur direction movements

--- Pacman.js
+++ Pacman.js
@@ -43,4 +43,35 @@ export default class Pacman {
         ];
         this.pacmanImageIndex = 0;
     }
+    #keydown = (event) => {
+        //up movement
+        if (event.keyCode == 38) {
+          if (this.currentMovingDirection == MovingDirection.down)
+            this.currentMovingDirection = MovingDirection.up;
+          this.requestedMovingDirection = MovingDirection.up;
+          this.madeFirstMove = true;
+        }
+        //down movement
+        if (event.keyCode == 40) {
+          if (this.currentMovingDirection == MovingDirection.up)
+            this.currentMovingDirection = MovingDirection.down;
+          this.requestedMovingDirection = MovingDirection.down;
+          this.madeFirstMove = true;
+        }
+        //left movement
+        if (event.keyCode == 37) {
+          if (this.currentMovingDirection == MovingDirection.right)
+            this.currentMovingDirection = MovingDirection.left;
+          this.requestedMovingDirection = MovingDirection.left;
+          this.madeFirstMove = true;
+        }
+        //right movement
+        if (event.keyCode == 39) {
+          if (this.currentMovingDirection == MovingDirection.left)
+            this.currentMovingDirection = MovingDirection.right;
+          this.requestedMovingDirection = MovingDirection.right;
+          this.madeFirstMove = true;
+        }
+      };
+
   }
