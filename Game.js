0bf6e47eceb20314f2a1d4dccd285c0c6018f3c1
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-27 18:07:45 +0300

    Rename Map into TileMap

--- Game.js
+++ Game.js
@@ -1,13 +1,12 @@
-import TileMap from "./Map.js";
+import TileMap from "./TileMap.js";
 
 const tileSize = 32;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const tileMap = new TileMap(tileSize);
 
-function gameloop ( ){ 
-    console.log("here");
-    tileMap.draw (ctx);
+function gameLoop() {
+    tileMap.draw(ctx);
 
 }
 
