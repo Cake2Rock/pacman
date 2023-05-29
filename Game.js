17c88d7a0d96dd4f56816fa61695ac7786f5ac03
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-29 21:35:10 +0300

    starting adding ghosts

--- Game.js
+++ Game.js
@@ -6,10 +6,13 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const tileMap = new TileMap(tileSize);
 const pacman = tileMap.getPacman(velocity);
+const enemies = tileMap.getEnemies(velocity);
 
 function gameLoop() {
     tileMap.draw(ctx);
+    tileMap.draw(ctx);
     pacman.draw(ctx, pause());
+    enemies.forEach((enemy) => enemy.draw(ctx, pause(), pacman));
 }
 
 tileMap.setCanvasSize(canvas);
