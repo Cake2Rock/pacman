b1e9141a5cace03e535172b22409b277245c5560
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-05-30 20:23:17 +0300

    Moving enemy

--- Enemy.js
+++ Enemy.js
@@ -1,3 +1,4 @@
+import MovingDirection from "./MovingDirection.js";
 export default class Enemy {
     constructor(x, y, tileSize, velocity, tileMap) {
         this.x = x;
@@ -18,7 +19,38 @@ export default class Enemy {
         this.scaredAboutToExpireTimerDefault = 10;
         this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
       }
-      draw(ctx) {}
+      draw(ctx) {
+        this.#move();
+        ctx.drawImage(this.image,this.x, this.y, this.tileSize, this.tileSize);
+      }
+      #move() {
+        if (
+          !this.tileMap.didCollideWithEnvironment(
+            this.x,
+            this.y,
+            this.movingDirection
+          )
+        ) {
+          switch (this.movingDirection) {
+            case MovingDirection.up:
+              this.y -= this.velocity;
+              break;
+            case MovingDirection.down:
+              this.y += this.velocity;
+              break;
+            case MovingDirection.left:
+              this.x -= this.velocity;
+              break;
+            case MovingDirection.right:
+              this.x += this.velocity;
+              break;
+          }
+        }
+      }
+    
+      #random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
   #loadImages() {
     this.normalGhost = new Image();
     this.normalGhost.src = "images/ghost.png";
