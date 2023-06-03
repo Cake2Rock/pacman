2f90bdf63e444f4d3ae6846893dd218f4bab433f
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2023-06-03 11:46:06 +0300

    colliding with enemy

--- src/Enemy.js
+++ src/Enemy.js
@@ -19,13 +19,53 @@ export default class Enemy {
         this.scaredAboutToExpireTimerDefault = 10;
         this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
       }
-      draw(ctx, pause) {
+      draw(ctx, pause, pacman) {
         if (!pause) {
           this.#move();
           this.#changeDirection();
         }
         ctx.drawImage(this.image,this.x, this.y, this.tileSize, this.tileSize);
       }
+
+      collideWith(pacman) {
+        const size = this.tileSize / 2;
+        if (
+          this.x < pacman.x + size &&
+          this.x + size > pacman.x &&
+          this.y < pacman.y + size &&
+          this.y + size > pacman.y
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    
+      #setImage(ctx, pacman) {
+        if (pacman.powerDotActive) {
+          this.#setImageWhenPowerDotIsActive(pacman);
+        } else {
+          this.image = this.normalGhost;
+        }
+        ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+      }
+    
+      #setImageWhenPowerDotIsActive(pacman) {
+        if (pacman.powerDotAboutToExpire) {
+          this.scaredAboutToExpireTimer--;
+          if (this.scaredAboutToExpireTimer === 0) {
+            this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
+            if (this.image === this.scaredGhost) {
+              this.image = this.scaredGhost2;
+            } else {
+              this.image = this.scaredGhost;
+            }
+          }
+        } else {
+          this.image = this.scaredGhost;
+        }
+      }
+    
       #move() {
         if (
           !this.tileMap.didCollideWithEnvironment(
--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,10 @@ const ctx = canvas.getContext("2d");
 const tileMap = new TileMap(tileSize);
 const pacman = tileMap.getPacman(velocity);
 const enemies = tileMap.getEnemies(velocity);
+let gameOver = false;
+let gameWin = false;
+const gameOverSound = new Audio("sounds/gameOver.wav");
+const gameWinSound = new Audio("sounds/gameWin.wav");
 
 function gameLoop() {
     tileMap.draw(ctx);
@@ -19,5 +23,30 @@ function gameLoop() {
     return !pacman.madeFirstMove || gameOver || gameWin;
   }
   
+
+  function checkGameWin() {
+    if (!gameWin) {
+      gameWin = tileMap.didWin();
+      if (gameWin) {
+        gameWinSound.play();
+      }
+    }
+  }
+  
+  function checkGameOver() {
+    if (!gameOver) {
+      gameOver = isGameOver();
+      if (gameOver) {
+        gameOverSound.play();
+      }
+    }
+  }
+  
+  function isGameOver() {
+    return enemies.some(
+      (enemy) => !pacman.powerDotActive && enemy.collideWith(pacman)
+    );
+  }
+
 tileMap.setCanvasSize(canvas);
 setInterval(gameLoop, 1000 / 75);
